0c94e489821f691c7bd2f248e96c6cfc1f603c3d
Michael <mrtcnc@mail.missouri.edu>
2020-02-11 12:40:36 -0600

    Initial Commit
    Pushing up all completed work

---
+++ DocumentChallenge/DocumentChallenge/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace DocumentChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string loop = "y";
+            while(loop.ToLower() == "y")
+            {
+                try
+                {
+                    Console.WriteLine("Document");
+
+                    Console.Write("Enter a name for the document: ");
+                    var name = Console.ReadLine();
+                    if (!name.EndsWith(".txt"))
+                    {
+                        name = $"{name}.txt";
+                    }
+
+                    Console.Write("Enter the document contents: ");
+                    var content = Console.ReadLine();
+                    var path = $"{Directory.GetParent(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName)}\\{name}";
+                    File.WriteAllText(path, content);
+
+                    Console.WriteLine($"{name} was successfully saved. The document contains {content.Length} characters.");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                finally
+                {
+                    Console.Write("Would you like to save another document? (y/n)");
+                    loop = Console.ReadLine();
+                }
+            }
+            
+            
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 1; i < 101; i++ )
+            {
+                if(i % 3 == 0)
+                {
+                    if(i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Fizz");
+
+                    }
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("\nPress enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
